6d5c9b39fa5fa83a9cdd8f29c9a773c21d8b7900
Tomas Hernandez <tohernandez@csumb.edu>
2017-06-24 22:21:38 -0700

    Refactored code to make card into seperate component

---
+++ src/components/CardComp.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import {Image, Text, View, StyleSheet, Button} from 'react-native';
+import {MKButton, MKColor} from 'react-native-material-kit';
+
+const ColorFab = MKButton.coloredFab().build();
+
+export default class CardComp extends Component {
+    constructor(props) {
+    super(props);
+  }
+  pushInfoButton = () => {
+    console.log("wot");
+  }
+  render() {
+    // <Text style={styles.text}>{card.price_level}</Text>
+    // <Text style={styles.text}>{card.rating}</Text>
+    // <TouchableHighlight onPress={() => navigate('Information')} underlayColor="white">
+    // </TouchableHighlight>
+    return (
+      <View style={styles.card}>
+        <View style={{alignItems: 'center', paddingTop: 10}}>
+          <Image
+            style={{width: 200, height: 200, borderRadius: 10}}
+            source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+          />
+          </View>
+        <Text style={styles.text}>{this.props.card.name}</Text>
+        <ColorFab
+            backgroundColor={'#F9F9EA'}
+            shadowRadius={1}
+            shadowOffset={{width:0, height:2}}
+            shadowOpacity={.6}
+            shadowColor="black"
+            style={styles.button}
+            onPress={this.pushInfoButton}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    backgroundColor: "white",
+  },
+  button: {
+    height: 20,
+    justifyContent: 'center',
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 20,
+    backgroundColor: "transparent",
+    paddingTop: 5,
+  }
+});
--- src/components/choicesScreen.js
+++ src/components/choicesScreen.js
@@ -4,6 +4,7 @@ import Config from 'react-native-config'
 import Swiper from 'react-native-deck-swiper';
 import Toast, {DURATION} from 'react-native-easy-toast'
 import {MKButton, MKColor} from 'react-native-material-kit';
+import CardComp from './CardComp';
 
 const ColorFab = MKButton.coloredFab().build();
 
@@ -19,36 +20,9 @@ export default class choicesScreen extends Component {
     };
   }
   renderCard = card => {
-    const { navigate } = this.props.navigation;
-    // <Text style={styles.text}>{card.price_level}</Text>
-    // <Text style={styles.text}>{card.rating}</Text>
-    // <TouchableHighlight onPress={() => navigate('Information')} underlayColor="white">
-    // </TouchableHighlight>
     return (
-      <View style={styles.card}>
-        <View style={{alignItems: 'center', paddingTop: 10}}>
-          <Image
-            style={{width: 200, height: 200, borderRadius: 10}}
-            source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
-          />
-          </View>
-        <Text style={styles.text}>{card.name}</Text>
-        <ColorFab
-            backgroundColor={'#F9F9EA'}
-            shadowRadius={1}
-            shadowOffset={{width:0, height:2}}
-            shadowOpacity={.6}
-            shadowColor="black"
-            style={styles.button}
-            onPress={this.pushInfoButton}
-        />
-      </View>
+      <CardComp card={card}/>
     );
-  }
-
-  pushInfoButton = () => {
-    const { navigate } = this.props.navigation;
-    navigate('Information');
   }
 
   onSwipedAllCards = () => {
@@ -75,6 +49,7 @@ export default class choicesScreen extends Component {
       cb
     );
   };
+
   swipeRight = () => {
     this.swipeBack();
     const { navigate } = this.props.navigation;
@@ -96,6 +71,7 @@ export default class choicesScreen extends Component {
     }
     return results;
   }
+
   componentDidMount(){
     navigator.geolocation.getCurrentPosition((position) => {
       fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' +
@@ -126,7 +102,6 @@ export default class choicesScreen extends Component {
   };
 
   render() {
-
     return (
      <View style={styles.container}>
        <Swiper
@@ -167,22 +142,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FF7F7F"
-  },
-  card: {
-    flex: 1,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: "#E8E8E8",
-    backgroundColor: "white",
-  },
-  button: {
-    height: 20,
-    justifyContent: 'center',
-  },
-  text: {
-    textAlign: "center",
-    fontSize: 20,
-    backgroundColor: "transparent",
-    paddingTop: 5,
   }
 });
