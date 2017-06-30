e677554a25603c938a1271b6792596dd47533e3f
Tomas Hernandez <tohernandez@csumb.edu>
2017-06-29 17:42:33 -0700

    Added a library that might fix image bug

--- android/app/src/main/java/com/blindbites/MainApplication.java
+++ android/app/src/main/java/com/blindbites/MainApplication.java
@@ -3,6 +3,7 @@ package com.blindbites;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FastImageViewPackage(),
             new VectorIconsPackage(),
             new ReactNativeConfigPackage(),
             new ReactMaterialKitPackage()
