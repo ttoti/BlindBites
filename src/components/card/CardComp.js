78d24e21b3a35d5c7a0ba7d4bc17c5121711b785
Tomas Hernandez <tohernandez@csumb.edu>
2017-07-22 13:43:44 -0700

    Cleaner looking button

--- src/components/card/CardComp.js
+++ src/components/card/CardComp.js
@@ -1,7 +1,8 @@
 'use strict';
 import React, {Component} from 'react';
-import {Dimensions, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
+import {Dimensions, Image, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {MKButton, MKColor} from 'react-native-material-kit';
 
 import CardPhotoComp from './CardPhotoComp';
 import DistanceComp from './DistanceComp';
@@ -10,25 +11,53 @@ import RatingComp from '../common/RatingComp';
 
 const { height } = Dimensions.get('window');
 const SCREEN_HEIGHT = height;
+const ColorFab = MKButton.coloredFab().build();
 
 export default class CardComp extends Component {
   constructor(props) {
     super(props);
   }
   render() {
+    // <ColorFab
+    //     backgroundColor={'#F9F9EA'}
+    //     shadowRadius={2}
+    //     shadowOffset={{width:0, height:2}}
+    //     shadowOpacity={.6}
+    //     shadowColor="black"
+    //     style={styles.button}
+    //     onPress={() => navigate('Choices')}
+    // >
+    //   <Image style={styles.image} pointerEvents="none" source={require('../assets/dice.png')} />
+    // </ColorFab>
+
+    // <TouchableHighlight style={styles.buttonView} onPress={() => this.props.infoCallback()}>
+    //   <View>
+    //     <Icon name="info" size={30} color="#FFFFFF"/>
+    //   </View>
+    // </TouchableHighlight>
     return (
       <View style={styles.outterView}>
         <View style={styles.card}>
           <View style={{alignItems: 'center', paddingTop: 10}}>
             <CardPhotoComp photo={this.props.card.photos}/>
           </View>
-          <View style={styles.button}>
+          <View style={{flex: .5, flexDirection: 'row', paddingTop: 15}}>
             <Text style={styles.text}>{this.props.card.name}</Text>
-            <TouchableHighlight style={styles.buttonView} onPress={() => this.props.infoCallback()}>
-              <View>
-                <Icon name="info" size={30} color="#FFFFFF"/>
-              </View>
-            </TouchableHighlight>
+            <View style={styles.button}>
+            <ColorFab
+                backgroundColor={'#FF7F7F'}
+                shadowRadius={1}
+                shadowOffset={{width:0, height:1}}
+                shadowOpacity={.4}
+                shadowColor="grey"
+                style={styles.button}
+                onPress={() => this.props.infoCallback()}
+                >
+                <View>
+                  <Icon name="info" size={30} color="#FFFFFF"/>
+                </View>
+            </ColorFab>
+            </View>
           </View>
           <View style={{flex: 1, flexDirection: 'row'}}>
             <View style={styles.cardColumn}>
@@ -66,18 +95,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   button: {
-    flex: 1,
+    height: 35,
+    width: 35,
+    justifyContent: 'center',
     flexDirection: 'row',
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  buttonView: {
-    backgroundColor: "#B4B4B4",
-    width: 40,
     alignItems: 'center',
-    borderWidth: .5,
-    borderColor: "#000000",
-    borderRadius: 50,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'contain'
   },
   outterView: {
     flex: 1,
@@ -85,10 +111,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   text: {
-    fontSize: 14,
+    fontSize: 18,
     backgroundColor: "transparent",
     paddingRight: 10,
-    paddingTop: 5,
+    paddingTop: 10,
   },
   cardColumn: {
     flex: 1,
